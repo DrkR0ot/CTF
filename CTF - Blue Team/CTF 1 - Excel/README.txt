b274368d1fda612b5d9e8991429aec5846f9396c
DarkR0ot <50878096+DrkR0ot@users.noreply.github.com>
2025-04-11 19:16:55 +0200

    Add files via upload

---
+++ CTF - Blue Team/CTF 1 - Excel/README.txt
@@ -0,0 +1,14 @@
+Pour faire le CTF :
+Contexte : 
+Vous êtes sur un PC isolé, vous pouvez lancer ce que vous voulez dessus MAIS vous n'avez pas accès a internet, autrement dit : NE PAS UTILISER LES OUTILS.
+Ici sera le challenge sera de progresser sur les outils natifs de Windows tel que PowerShell.
+
+Etape 1 :
+	- Copier-Coller le CTF sur votre bureau
+Etape 2 : 
+	- Ouvrir l'explorateur de fichier et rendez-vous à l'emplacement ou vous avez collé le CTF
+Etape 3 :
+	- Clic droit > propriété > (tout en bas) cocher la case "débloquer" de la catégorie "sécurité" (si une GPO vous bloque l'exécution)
+Etape 4 :
+	- Lancer le CTF
+Have Fun !
--- CTF - Blue Team/CTF 1 - Excel/Script.ps1
+++ CTF - Blue Team/CTF 1 - Excel/Script.ps1
@@ -1,1 +1,16 @@
-
+﻿Add-Type -AssemblyName System.Windows.Forms
+Invoke-item "\\Mon_Serveur_De_Fichiers_Partagés\Anonymous.jpg"
+Start-Process Wordpad
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("Attention à ce que vous ouvrez ! J ai collé un fichier sur votre environnement USERPROFILE. A vous de le retrouver :3")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+[System.Windows.Forms.SendKeys]::SendWait("Questions :")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+[System.Windows.Forms.SendKeys]::SendWait("1 - Que fait le fichier Excel ?")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+[System.Windows.Forms.SendKeys]::SendWait("2 - Que font les scripts ?")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+[System.Windows.Forms.SendKeys]::SendWait("3 - Quelles sont les étapes d'investigations ?")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+[System.Windows.Forms.SendKeys]::SendWait("4 - Proposez des remédiations")
+xcopy "\\Mon_Serveur_De_Fichiers_Partagés\Popup.cmd" "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /H
